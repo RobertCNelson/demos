1c88c29281662b5d94bee495a7b7865e0e9e4702
Robert Nelson <robertcnelson@gmail.com>
2016-05-13 12:30:42 -0500

    devie1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- nxp-ftp-2016/example.sh
+++ nxp-ftp-2016/example.sh
@@ -8,13 +8,13 @@ send_data () {
 #        echo -e "\$${data}*${date_cksum}#\n" > ${port}
 }
  
-if [ ! -f /sys/bus/iio/devices/iio:device0/in_temp_raw ] ; then
+if [ ! -f /sys/bus/iio/devices/iio:device1/in_temp_raw ] ; then
     echo si7005 0x40 > /sys/bus/i2c/devices/i2c-1/new_device
 fi
  
-in_temp_raw=$(cat /sys/bus/iio/devices/iio:device0/in_temp_raw || true)
-in_temp_offset=$(cat /sys/bus/iio/devices/iio:device0/in_temp_offset || true)
-in_temp_scale=$(cat /sys/bus/iio/devices/iio:device0/in_temp_scale || true)
+in_temp_raw=$(cat /sys/bus/iio/devices/iio:device1/in_temp_raw || true)
+in_temp_offset=$(cat /sys/bus/iio/devices/iio:device1/in_temp_offset || true)
+in_temp_scale=$(cat /sys/bus/iio/devices/iio:device1/in_temp_scale || true)
   
 if [ ! "x${in_temp_raw}" = "x" ] && [ ! "x${in_temp_offset}" = "x" ] && [ ! "x${in_temp_scale}" = "x" ] ; then
     temp=$(echo "${in_temp_raw} + ${in_temp_offset}" | bc)
