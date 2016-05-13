4e3a9891406df53de262c11adb440f997213a783
Robert Nelson <robertcnelson@gmail.com>
2016-05-13 13:23:22 -0500

    add humdity
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- nxp-ftp-2016/example.sh
+++ nxp-ftp-2016/example.sh
@@ -27,3 +27,14 @@ if [ ! "x${in_temp_raw}" = "x" ] && [ ! "x${in_temp_offset}" = "x" ] && [ ! "x${
 	send_data
 fi
 
+in_hum_raw=$(cat /sys/bus/iio/devices/iio:device1/in_humidityrelative_raw || true)
+in_hum_offset=$(cat /sys/bus/iio/devices/iio:device1/in_humidityrelative_offset || true)
+in_hum_scale=$(cat /sys/bus/iio/devices/iio:device1/in_humidityrelative_scale || true)
+
+if [ ! "x${in_hum_raw}" = "x" ] && [ ! "x${in_hum_offset}" = "x" ] && [ ! "x${in_hum_scale}" = "x" ] ; then
+	hum=$(echo "${in_hum_raw} + ${in_hum_offset}" | bc)
+	hum=$(echo "scale=5; ${hum} * ${in_hum_scale}" | bc)
+	hum=$(echo "scale=5; ${hum} / 1000" | bc)
+	echo ${hum}
+fi
+
